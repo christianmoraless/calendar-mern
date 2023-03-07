cbf38015896ff78acb07029a8abeacd3be8cd75a
Christian Morales <christiandoraless@gmail.com>
2023-03-07 19:54:07 -0400

    feat:adding calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,3 +1,45 @@
+import { Navbar } from "../components/Navbar";
+// Calendar and dates
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import enUS from "date-fns/locale/en-US";
+
+// Lenguaje
+const locales = {
+  "en-US": enUS,
+};
+// Localizer
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Cumpleaños de Camila",
+    notes: "Hay que comprar la torta",
+    start: new Date(),
+  },
+];
+
 export const CalendarPage = () => {
-  return <div>CalendarPage</div>;
+  return (
+    <>
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </>
+  );
 };
