6ce9494ae1bee526edc85a0aea4ccef14d0565e2
christianmoraless <christiandmoraless@gmail.com>
2023-04-21 23:00:40 -0400

    fix bug

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -8,6 +8,7 @@ export const Navbar = () => {
       <span className="navbar-brand">
         <i className="fas fa-calendar-alt"></i>
         &nbsp;
+        {user.name}
       </span>
       <button className="btn btn-outline-danger" onClick={startLogout}>
         <i className="fas fa-sign-out-alt"></i>
