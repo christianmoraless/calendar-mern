5e89c3f7eaac7b2864bf7d25b1467f4a0667730e
christianmoraless <christiandmoraless@gmail.com>
2023-04-23 19:26:00 -0400

    feat:adding backend url

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -4,7 +4,7 @@ import { getEnvVariables } from "../helpers";
 const { VITE_API_URL } = getEnvVariables();
 
 export const calendarApi = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: "https://calendar-backend-tan.vercel.app/",
 });
 
 calendarApi.interceptors.request.use((config) => {
